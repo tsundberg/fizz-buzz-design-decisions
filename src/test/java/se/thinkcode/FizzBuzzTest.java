4f3aa9542ec26853e2216135974bdcec402b4c12
Thomas Sundberg <tsu@kth.se>
2015-06-26 11:25:00 +0200

    project setup.

---
+++ src/test/java/se/thinkcode/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package se.thinkcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void verify_infra_structure(){
+        assertTrue(true);
+    }
+}
